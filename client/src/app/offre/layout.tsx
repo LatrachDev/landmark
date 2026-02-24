415671c058fc1883f399a10dcdf54a1a35949a0e
Mohammed Latrach <m.latrach.youcode@gmail.com>
2026-02-24 20:32:11 +0000

    LAN-21 Offres Contenu

---
+++ client/src/app/offre/layout.tsx
@@ -0,0 +1,17 @@
+export default function OffreLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            {children}
+            <style>{`
+                body > footer,
+                body > div > footer {
+                    display: none !important;
+                }
+            `}</style>
+        </>
+    );
+}
